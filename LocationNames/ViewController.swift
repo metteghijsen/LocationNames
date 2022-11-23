017e3d278ee7e55187d157fdc50b9c82802ed140
metteghijsen <mette.ghijsen@gmail.com>
2022-11-23 10:50:31 +0100

    Start gemaakt aan locatie POC

--- LocationNames/ViewController.swift
+++ LocationNames/ViewController.swift
@@ -5,15 +5,39 @@
 //  Created by Mette Ghijsen on 23/11/2022.
 //
 
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let map: MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(map)
+        
+        LocationManager.shared.getUserLocation {[weak self] location in
+            DispatchQueue.main.async {
+                guard let strongSelf = self else {
+                    return
+                }
+                
+                let pin = MKPointAnnotation()
+                pin.coordinate = location.coordinate
+                strongSelf.map.setRegion(MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.7, longitudeDelta: 0.7)), animated: true)
+                strongSelf.map.addAnnotation(pin)
+            }
+        }
+        
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        map.frame = view.bounds
+    }
 
 }
 
